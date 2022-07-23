db38aae30ec7af64fde89ec42440fb2775f23751
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-07-23 12:41:27 -0400

    contact/portfolio start

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Portfolio(props) {
+  const { currentCategory } = props;
+  return (
+    <section>
+      <p>test text</p>  
+    </section>
+  );
+}
+export default Portfolio;
