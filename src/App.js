4ccc0fe136cb55f369f669f031e957ca40c58202
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-07-23 11:48:17 -0400

    app/about/nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
+  const [categories] = useState([ 
+    {
+      name: 'about',
+      description: 'About me, relevant personal information',
+    },
+    { name: 'portfolio', description: 'Portfolio of applications and class work' },
+    { name: 'contact', description: 'Contact information' },
+    { name: 'resume', description: 'My resume and professional information' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Portfolio currentCategory={currentCategory}></Portfolio>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
+      </main>
     </div>
   );
 }
