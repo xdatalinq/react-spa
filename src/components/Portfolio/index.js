09585fbfa9a1c9e308e2981323928a6d1d811346
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-07-25 11:21:50 -0400

    portfolio work updated, working images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,59 +1,93 @@
 import React from 'react';
+import runbuddy from '../../assets/images/runbuddy.png';
+import secbox1 from '../../assets/images/secondarybox1.jpg';
+import secbox2 from '../../assets/images/secondarybox2.jpg';
+import secbox3 from '../../assets/images/secondarybox3.jpg';
+import secbox4 from '../../assets/images/secondarybox4.jpg';
+import secbox5 from '../../assets/images/secondarybox5.jpg';
 
 function Portfolio(props) {
   const { currentCategory } = props;
   return (
-    <section id="work">
-      <div class="flex-container">
-        <h2 class="section-title">
+    <section data-testid="work">
+      <div className="flex-container">
+        <h2 className="section-title">
           Work
         </h2>
-        <div class="section-border">
-          <article class="box-wrapper">
-            <div class="secondary-box">
+        <div className="section-border">
+          <article className="box-wrapper">
+            <div className="secondary-box">
               <div>
                 <a href="https://xdatalinq.github.io/run-buddy/" target="_blank">
-                <img src="./assets/images/runbuddy.png" alt="Run Buddy website preview"></img>
+                <img src={runbuddy} alt="Run Buddy website preview"/>
                 </a>
               </div>
-              <div class="secondary-title">
+              <div className="secondary-title">
                 <h3>Run Buddy</h3>
                 <p>HTML/CSS</p>
               </div>
            </div>
          </article>
-          <article class="box-wrapper">
-            <div class="secondary-box">
+          <article className="box-wrapper">
+            <div className="secondary-box">
              <div>
                 <a href="https://residential-matchmaker.herokuapp.com/" target="_blank">
-                <img src="./assets/images/secondarybox5.jpg" alt="Residential Matchmaker website preview"></img>
+                <img src={secbox5} alt="Residential Matchmaker website preview"></img>
                 </a>
               </div>
-              <div class="secondary-title">
+              <div className="secondary-title">
                 <h3>Residential Matchmaker</h3>
                <p>Javascript/Express/Handlebars</p>
               </div>
            </div>
           </article>
-          <article class="box-wrapper">
-            <div class="secondary-box">
+          <article className="box-wrapper">
+            <div className="secondary-box">
               <div>
-                <img src="./assets/images/secondarybox3.jpg" alt="Calculator website preview"></img>
+                <a href="https://damp-hamlet-71190.herokuapp.com/" target="_blank">
+                <img src={secbox3} alt="Calculator website preview"></img>
+                </a>
               </div>
-              <div class="secondary-title">
+              <div className="secondary-title">
+                <h3>Budget Tracker</h3>
+                <p>Progressive Web App</p>
+              </div>
+            </div>
+          </article>
+          <article className="box-wrapper">
+            <div className="secondary-box">
+              <div>
+                <a href="https://damp-hamlet-71190.herokuapp.com/" target="_blank">
+                <img src={secbox4} alt="Pastel puzzles website preview"></img>
+                </a>
+              </div>
+              <div className="secondary-title">
+                <h3>Social Media Network API</h3>
+                <p>Express</p>
+              </div>
+            </div>
+          </article>
+          <article className="box-wrapper">
+            <div className="secondary-box">
+              <div>
+                <img src={secbox2} alt="Calculator website preview"></img>
+              </div>
+              <div className="secondary-title">
                 <h3>Calculator</h3>
                 <p>React/JavaScript/CSS</p>
               </div>
             </div>
           </article>
-          <article class="box-wrapper">
-            <div class="secondary-box">
+          <article className="box-wrapper">
+            <div className="secondary-box">
               <div>
-                <img src="./assets/images/secondarybox4.jpg" alt="Pastel puzzles website preview"></img>
+                <a href="https://github.com/xdatalinq/react-spa" target="_blank">
+                <img src={secbox1} alt="Pastel puzzles website preview"></img>
+                </a>
               </div>
-              <div class="secondary-title">
-                <h3>Pastel Puzzles</h3>
-                <p>MERN Stack</p>
+              <div className="secondary-title">
+                <h3>Single Page Application</h3>
+                <p>React/JSX</p>
               </div>
             </div>
           </article>
